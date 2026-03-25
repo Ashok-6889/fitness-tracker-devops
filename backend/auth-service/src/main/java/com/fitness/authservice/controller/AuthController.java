355828081e406f6fe1a1804ce6652af4f6641dfe
Ashok-6889 <ashoknelluri2nd@gmail.com>
2026-03-25 08:36:10 +0530

    Create AuthController.java

---
+++ backend/auth-service/src/main/java/com/fitness/authservice/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.fitness.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Auth Service Working 🚀";
+    }
+}
